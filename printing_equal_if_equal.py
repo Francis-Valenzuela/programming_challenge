b00509bde5b7bcdda305d806e9ab906a56831115
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 12:43:43 +0800

    [200~conditional statement + space is needed to compare

--- printing_equal_if_equal.py
+++ printing_equal_if_equal.py
@@ -1,3 +1,3 @@
 #2. Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
-num_1,num_2 = map(float, input("Enter two Numbers:"))
-
+num_1,num_2 = map(float, input("Enter two Numbers:"),split())
+print('Equal' if num_1 == num_2 else 'Not Equal')
