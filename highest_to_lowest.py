a95e3a369fde0bd68957e80735d1b686c05d9f9b
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 21:51:39 +0800

    using reverse sort to arrange highest to lowest

--- highest_to_lowest.py
+++ highest_to_lowest.py
@@ -1,6 +1,10 @@
+sorted_numbers = []
 while True:
     num = input("Enter a number: ")
 
     if not num.isdigit():
         break
     num = int(num)
+    sorted_numbers.append(num)
+sorted_numbers.sort(reverse=True)
+print(sorted_numbers)
