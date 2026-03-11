264a1a92911920cc6171af65b4501e47ae374f2d
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 17:24:49 +0800

    using loop with start + 1 and stop for num2

--- between_two_numbers.py
+++ between_two_numbers.py
@@ -1,1 +1,3 @@
-num1, num2 = int(input("Enter a Value")), int(input("Enter another Value"))
+num1, num2 = int(input("Enter a Value:")), int(input("Enter another Value:"))
+for num in range(num1 + 1, num2 + 1):
+    print(num, end=' ')
