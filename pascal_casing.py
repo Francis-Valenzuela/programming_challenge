766cc947d4266fb624b4f3ee9eb458c8f3eccee6
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 23:53:24 +0800

    use split to identify each word, capitalize to caps 1st letter per word, use pascal_full_name variable to store the += word in the loop

--- pascal_casing.py
+++ pascal_casing.py
@@ -1,2 +1,8 @@
 full_name = input('Enter a full name: ')
-print(full_name)
+full_name = full_name.split()
+pascal_full_name = ''
+
+for words in full_name:
+    pascal_full_name += words.capitalize()
+
+print(pascal_full_name)
