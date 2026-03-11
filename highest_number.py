cf2bd8d9c7c2b7b7775f7d7f941359c64e6f573a
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 21:39:54 +0800

    conditional statement to get highest number

--- highest_number.py
+++ highest_number.py
@@ -1,8 +1,16 @@
+highest = None
+
 while True:
     num = input('Enter a number: ')
 
     if not num.isdigit():
         break
-    
+    num = int(num)
+
+    if highest is None or num > highest:
+        highest = num
+print(highest)
+
+
 
 
