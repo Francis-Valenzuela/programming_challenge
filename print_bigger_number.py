6f3edd27b23ab54730ce3ddf063968e7c7f553d8
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 12:27:34 +0800

    printing bigger number

---
+++ print_bigger_number.py
@@ -0,0 +1,6 @@
+#1. Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+# ask 2 input
+num_1,num_2 = map(float, input("Enter two Numbers:").split())
+print(max(num_1,num_2))
+
+
