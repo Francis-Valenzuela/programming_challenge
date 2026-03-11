af51636aef443ca5aae8e03191eac9a889f9150d
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-11 22:15:40 +0800

    formatted into 6 digits

--- number_formatting.py
+++ number_formatting.py
@@ -1,5 +1,5 @@
 num = int(input("Enter a number (0-1000): "))
 while 0 > num or num > 1000:
     num = int(input("Enter a number (0-1000): "))
-formatted_num = f"{num}"
+formatted_num = f"{num:06d}"
 print(formatted_num)
