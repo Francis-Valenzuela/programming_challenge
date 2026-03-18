1ef73e0b3a5c761682c1c58fb101761051456646
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-18 14:57:54 +0800

    statement if the first letter in a NEW WORD = true is small case, it will convert to capital

--- title_casing.py
+++ title_casing.py
@@ -6,3 +6,6 @@ for char in value:
     if char == ' ':
         result  += char
         new_word =  True
+    elif new_word and 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)
+        new_word = False
