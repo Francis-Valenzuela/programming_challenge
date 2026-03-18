6bf448e0326e310946660920c547c233c1ad1441
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-18 23:38:49 +0800

    the loop will start to check the end of the string from left to right. If the letter was found at a certain index, it will stop and save the index where the letter was detected. Then it will be printed.

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -1,2 +1,9 @@
 value = input("Enter a String:")
 letter =  input("Enter a Letter:")
+position = -1
+
+for i in range(len(value)-1, -1, -1):
+    if value[i] == letter:
+        position = i
+        break
+print(position)
