27c7cddbbda54b5464c45b7739d11c3fa554aa69
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-18 14:46:49 +0800

    check if there is an uppercase, if  there is, it will be converted to small case

--- first_letter_uppercase.py
+++ first_letter_uppercase.py
@@ -4,3 +4,5 @@ result = ""
 for i in range(len(value)):
     if i == 0 and 'a' <= value[i] <= 'z':
         result += chr(ord(value[i]) -32 )
+    elif 'A' <= value[i] <= 'Z':
+        result += chr(ord(value[i]) + 32)
