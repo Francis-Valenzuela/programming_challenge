f938219fc3b2d839935e95b1e25f005071df5646
Francis-Valenzuela <francispvalenzuela@iskolarngbayan.pup.edu.ph>
2026-03-18 14:48:48 +0800

    else statement is if the input is already in the desired format, then print

--- first_letter_uppercase.py
+++ first_letter_uppercase.py
@@ -6,3 +6,6 @@ for i in range(len(value)):
         result += chr(ord(value[i]) -32 )
     elif 'A' <= value[i] <= 'Z':
         result += chr(ord(value[i]) + 32)
+    else:
+        result += value[i]
+print(result)
